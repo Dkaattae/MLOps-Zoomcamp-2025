8df61276ba0802e8103aa8c0eee044d36dc24649
XueyingChen <115363697+Dkaattae@users.noreply.github.com>
2025-05-13 20:28:43 +0000

    experiment tracking update

--- experiment_tracking/train.py
+++ experiment_tracking/train.py
@@ -9,7 +9,7 @@ import mlflow
 from mlflow.models.signature import infer_signature
 
 
-mlflow.set_tracking_uri("sqlite:///mlflow.db")
+mlflow.set_tracking_uri("http://127.0.0.1:5000")
 mlflow.set_experiment("nyc-taxi-experiment")
 
 
--- orchestration/orchestrate.py
+++ orchestration/orchestrate.py
@@ -1,54 +1,58 @@
-from experiment_tracking import preprocess_data
-from experiment_tracking import train
-from experiment_tracking import hpo
-from experiment_tracking import register_model
 import mlflow
+import subprocess
 from prefect import flow, task
 
 
 @task(retries=3, retry_delay_seconds=2)
-def preprocess_task() -> None:
+def preprocess_task(raw_data_path, dest_path) -> None:
     '''
     dump four pickle files into output folder 
     dictVectorizer from training, X_train, X_val, X_test
     '''
-    preprocess_data.run_data_prep()
+    subprocess.run([
+        "python",
+        "experiment_tracking/preprocess_data.py",
+        "--raw_data_path", raw_data_path,
+        "--dest_path", dest_path
+    ], check=True)
 
 @task()
 def train_task():
     '''
     load pickles, run model on validation set, autolog in mlflow
     '''
-    run_train()
+    subprocess.run(["python", "experiment_tracking/train.py"], check=True)
 
 @task()
-def hyperopt_task():
+def hyperopt_task(data_path):
     '''
     start mlflow server, searching best pamameters based on val rmse, logging in mlflow
     '''
-    run_optimization()
+    # add data_path to experiment_tracking/output instead of default ./output
+    subprocess.run(["python", "experiment_tracking/hpo.py", "--data_path", data_path], check=True)
 
 @task()
-def register_task():
+def register_task(data_path):
     '''
     find top models from hyperopt, run model on test dataset, logging in mlflow, register the best on test set.
     '''
-    run_register_model()
+    subprocess.run(["python", "experiment_tracking/register_model.py", "--data_path", data_path], check=True)
 
 @flow()
-def main_flow():
+def main_flow(raw_data_path, dest_path):
     """The main training pipeline"""
 
-    # MLflow settings
-    mlflow.set_tracking_uri("sqlite:///mlflow.db")
-    mlflow.set_experiment("nyc-taxi-experiment")
-
-    preprocess_task()
+    preprocess_task(raw_data_path, dest_path)
     # train_task()
-    hyperopt_task()
-    register_task()
+    hyperopt_task(dest_path)
+    register_task(dest_path)
 
 
 
 if __name__ == "__main__":
-    main_flow()
+    import argparse
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--raw_data_path", required=True)
+    parser.add_argument("--dest_path", default='./experiment_tracking/output')
+    args = parser.parse_args()
+    main_flow(args.raw_data_path, args.dest_path)
